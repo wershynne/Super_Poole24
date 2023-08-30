1ac61a159a48fc2a3aa89cd1d6109ea4dc3fe065
DaKahd <dakahd6@gmail.com>
2023-08-30 13:19:48 -0700

    Implemeted stopwatch class

--- 002_BigO/base_code/main.java
+++ 002_BigO/base_code/main.java
@@ -5,34 +5,23 @@ import java.lang.*;
 
 
 class main {
-	static final long createdNano = System.nanoTime();
-
 	public static void main(String args[]) {
-		long start = System.nanoTime();
-		long finish = System.nanoTime();
-		System.out.println("Created: " + createdNano);
-		
 		int [] times = {10, 100, 1000, 10000, 100000, 1000000, 10000000};
 		int [] nums = new int[100];
-
+		Stopwatch s = new Stopwatch();
 
 		System.out.println("-------------------Test-------------------");
 		System.out.println("");
 		for(int i : times){
 			System.out.println("Interval: " + i);
-			
+			s.start();	
 			nums = new int[i];
-			start = System.nanoTime();
-
 			//  Put your method between these two comments
-
 			//
-
-			finish = System.nanoTime();
-			System.out.println("Started: " + start);
-			System.out.println("Finished: " + finish);
-			System.out.println("Duration: " + (finish-start));
+			s.stop();
+			System.out.println("Duration: " + Stopwatch.readable(s.read()));
 			System.out.println("");
 		}
 	}
 }
+
